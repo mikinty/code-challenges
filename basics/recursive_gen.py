c394641219b38cb964510bbdb6ef6783c9b25b16
mikinty <youmichaelc@gmail.com>
2022-04-12 03:54:38 -0400

    nQueens solution
    Just working through 2.2 rn

--- basics/recursive_gen.py
+++ basics/recursive_gen.py
@@ -66,9 +66,48 @@ def generateAllPermutations(n):
     return generateAllPermutationsHelper(n, l, chosen)
 
 
+def nQueensHelper(row, board):
+    if row == len(board):
+        for i in range(len(board)):
+            print(''.join([str(x) for x in board[i]]))
+        print(''.join(['-' for i in range(len(board))]))
+    else:
+        def inRange(row, col, board):
+            return row >= 0 and col >= 0 and row < len(board) and col < len(board[0])
+
+        def noQueensBehind(row, col, board):
+            # Check rows behind
+            for delta in range(1, row + 1):
+                if board[row - delta][col] != 0:
+                    return False
+                if inRange(row - delta, col - delta, board) and board[row - delta][col - delta] != 0:
+                    return False
+                if inRange(row - delta, col + delta, board) and board[row - delta][col + delta] != 0:
+                    return False
+
+            return True
+
+        for col in range(len(board)):
+            if noQueensBehind(row, col, board):
+                board[row][col] = 'Q'
+                nQueensHelper(row + 1, board)
+                board[row][col] = 0
+
+
+def nQueens(n):
+    '''
+    Generates all possible nxn boards where n queens do not attack any of
+    themselves
+    '''
+    board = [[0 for j in range(n)] for i in range(n)]
+
+    nQueensHelper(0, board)
+
+
 '''
 Test all of our functions out
 '''
 # buildAllSubsets(4)
 # generateAllFullSets(4)
-generateAllPermutations(4)
+# generateAllPermutations(4)
+nQueens(5)
