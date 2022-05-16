5951c3125422098ff5e67ef262c702fd71154f2e
mikinty <youmichaelc@gmail.com>
2022-05-15 23:12:38 -0400

    Challenge 24 with no print solution as well, more readable

--- 14 - Challenge 24/myou.py
+++ 14 - Challenge 24/myou.py
@@ -1,4 +1,34 @@
 class Solution:
+    def judgeCardsNoPrint(self, cards):
+        if len(cards) == 0:
+            return False  # this should never be the case
+        elif len(cards) == 1:
+            return abs(cards[0] - 24) < 1e-9
+        else:
+            # try all combinations (choose) of two numbers in the cards,
+            # calculate all possible values they can have, and recurse on them
+            # plus the remaining nucards
+            for i in range(len(cards)):
+                for j in range(i+1, len(cards)):
+                    a = cards[i]
+                    b = cards[j]
+
+                    rem_numbers = [cards[x] for x in range(
+                        len(cards)) if (x != i and x != j)]
+
+                    possible = set([a-b, b-a, a*b, a+b])
+
+                    if a != 0:
+                        possible.add(b/a)
+                    if b != 0:
+                        possible.add(a/b)
+
+                    for val in possible:
+                        if self.judgeCardsNoPrint(rem_numbers + [val]):
+                            return True
+
+            return False
+
     def judgeCards(self, cards, currExp={}, possibleExpr=''):
         if len(cards) == 0:
             return False  # this should never be the case
@@ -54,8 +84,11 @@ class Solution:
             return False
 
     def judgePoint24(self, cards):
-        return self.judgeCards(cards)
+        return self.judgeCardsNoPrint(cards)
 
 
 sol = Solution()
 print(sol.judgePoint24([8, 3, 8, 3]))
+print(sol.judgePoint24([8, 3, 1, 1]))
+print(sol.judgePoint24([1, 4, 5, 6]))
+print(sol.judgePoint24([100, 3, 54, 7]))
